0707bad0158054dcefeea8e2475ff4f03ca9d26f
Shubham D <shubhamd@scriptlanes.com>
2022-10-10 15:08:54 +0530

    Excel changes

--- src/app/sheet/sheet.component.ts
+++ src/app/sheet/sheet.component.ts
@@ -1,57 +1,34 @@
-import { Component, OnInit } from '@angular/core';
-import * as XLSX from 'xlsx';
-
+import { Component, OnInit } from "@angular/core";
+import * as XLSX from "xlsx";
 
 @Component({
-  selector: 'app-sheet',
-  templateUrl: './sheet.component.html',
-  styleUrls: ['./sheet.component.css']
+  selector: "app-sheet",
+  templateUrl: "./sheet.component.html",
+  styleUrls: ["./sheet.component.css"],
 })
-
 export class SheetComponent implements OnInit {
-  answer = 'No answer yet';
+  answer = "No answer yet";
   a: any;
-
 
   ExcelData: any;
   ans: any;
-  //in: any
-  constructor() {
-
-  }
-  ngOnInit() {
-
-
-
-  }
+  constructor() {}
+  ngOnInit() {}
   ReadExcel(event: any) {
     let file = event.target.files[0];
     let fileReader = new FileReader();
     fileReader.readAsBinaryString(file);
     fileReader.onload = (e) => {
-      var workbook = XLSX.read(fileReader.result, { type: 'binary' });
-      var SheetNames = workbook.SheetNames;
-      this.ExcelData = XLSX.utils.sheet_to_json(workbook.Sheets[SheetNames[0]]);
-      console.log(this.ExcelData);
-    }
+      let workbook = XLSX.read(fileReader.result, { type: "binary" });
+      let SheetNames = workbook.SheetNames;
+      const data: any[] = XLSX.utils.sheet_to_json(
+        workbook.Sheets[SheetNames[0]]
+      );
+      this.ExcelData = data.map((res) => ({ ...res, userSelected: "" }));
+    };
   }
-  change(data: any,index:any) {
-    console.log("Data", index);
-
-    for (let i = 0; i <= this.ExcelData.length; i++) {
-    //  console.log('answers:', this.ExcelData[i])
-
-      if (this.ExcelData[i] != null) {
-        if (this.ExcelData[i].correctanswer == data )  {
-          console.log("your answer is correct");
-          this.ans= this.ExcelData[i].correctanswer;
-          console.log("your answer is correct",this.ans);
-        }else{
-
-          this.ans="wrong answer";
-        }
-      }
-    }
+  change(data: any, index: any) {
+    this.ExcelData[index].userSelected = data;
   }
-
+  onSubmitHandler() {}
 }
